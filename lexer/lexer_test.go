dd42072ad5732d63615d314dc6a4d5c735a6c148
Abiria <abiria.dev@gmail.com>
2024-03-06 09:39:20 +0900

    feat: add a test

---
+++ lexer/lexer_test.go
@@ -0,0 +1,19 @@
+package lexer
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHello(t *testing.T) {
+	lexer := NewCleaLexer("(+ 1 2)")
+
+	assert := assert.New(t)
+
+	assert.Equal(lexer.Scan(), "(")
+	assert.Equal(lexer.Scan(), "+")
+	assert.Equal(lexer.Scan(), "1")
+	assert.Equal(lexer.Scan(), "2")
+	assert.Equal(lexer.Scan(), ")")
+}
