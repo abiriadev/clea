22208005199f4bcba67be35857e296dd3c2f5612
Abiria <abiria.dev@gmail.com>
2024-03-06 23:08:44 +0900

    feat: implement `Push` and `Insert` methods

--- parser/stack.go
+++ parser/stack.go
@@ -1,13 +1,24 @@
 package parser
 
-import "github.com/abiriadev/clea/lexer"
+import (
+	"github.com/abiriadev/clea/lexer"
+	"github.com/samber/mo"
+)
 
 type TokenStack struct {
-	stack []lexer.Token
+	stack []Tree
 }
 
 func NewTokenStack() TokenStack {
 	return TokenStack{
-		stack: make([]lexer.Token, 0),
+		stack: make([]Tree, 0),
 	}
+}
+
+func (s *TokenStack) Push() {
+	s.stack = append(s.stack, Tree(mo.Right[lexer.Token](make([]Tree, 0))))
+}
+
+func (s *TokenStack) Insert(token lexer.Token) {
+	s.stack = append(s.stack, Tree(mo.Left[lexer.Token, []Tree](token)))
 }
