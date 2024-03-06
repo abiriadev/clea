9c015313ad1ad3be979c37f064d162d95f65ac18
Abiria <abiria.dev@gmail.com>
2024-03-06 23:37:57 +0900

    refactor: reuse code

--- parser/stack.go
+++ parser/stack.go
@@ -24,10 +24,17 @@ func (s *TokenStack) Insert(token lexer.Token) {
 	s.stack[l-1] = append(s.stack[l-1], Tree(mo.Left[lexer.Token, []Tree](token)))
 }
 
-func (s *TokenStack) Pop() {
+func (s *TokenStack) Shift() {
+	last := s.Pop()
+	l := len(s.stack)
+
+	s.stack[l-1] = append(s.stack[l-1], Tree(mo.Right[lexer.Token](last)))
+}
+
+func (s *TokenStack) Pop() []Tree {
 	l := len(s.stack)
 	last := s.stack[l-1]
 	s.stack = s.stack[:l-1]
 
-	s.stack[l-2] = append(s.stack[l-2], Tree(mo.Right[lexer.Token](last)))
+	return last
 }
